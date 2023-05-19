10e39c6e577913b3d49488d4360ac238049d7ef9
Husnain.Arshad <husnain.arshad@invozone.com>
2023-05-19 14:52:32 +0500

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `My-Test-Project`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
